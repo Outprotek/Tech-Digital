5a3e02a9ca84f5b3cc1364efb0e95c40461727a4
Wahyu Rifia Rizki <wahyurifia927@gmail.com>
2025-02-19 00:38:14 +0700

    add crud variant

---
+++ src/services/variantServices.ts
@@ -0,0 +1,53 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const finds = async () => {
+  const totalData = await prisma.variant.count();
+  const response = await prisma.variant.findMany();
+  return { response, totalData };
+};
+
+const findById = async (id: number) => {
+  const response = await prisma.variant.findFirst({
+    where: {
+      id,
+    },
+  });
+  if (!response) throw Error("variant not found");
+  return response;
+};
+
+const findByProductId = async (productId: string) => {
+  const totalData = await prisma.variant.count({
+    where: {
+      productId,
+    },
+  });
+  const response = await prisma.variant.findMany({
+    where: {
+      productId,
+    },
+  });
+  if (!response) throw Error("variant not found by productId");
+  return { response, totalData };
+};
+
+const add = async (data: Prisma.VariantCreateInput) => {
+  const response = await prisma.variant.create({
+    data: {
+      ...data,
+    },
+  });
+  return response;
+};
+
+const remove = async (id: number) => {
+  await prisma.variant.delete({
+    where: {
+      id,
+    },
+  });
+};
+
+export default { finds, findById, findByProductId, add, remove };
